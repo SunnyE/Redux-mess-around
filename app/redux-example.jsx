508a1c97a746965ee87c5948b22a9396270c76f7
Ethan Favia <efavia215@live.com>
2017-01-27 15:39:33 -0500

    movies reducer pulled out of bigger reducer

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -9,56 +9,56 @@ var startingState = {
 }
 var nextHobbyId = 1; 
 var nextMovieId = 1;
-var oldReducer = (state = startingState, action) => {
-    // state = state || {name: 'Annonymous'};
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return {
-                ...state, 
-                name: action.name
-            };
-        case 'ADD_HOBBY':
-            return {
-                ...state,
-                hobbies: [
-                    ...state.hobbies,
-                    {
-                        id: nextHobbyId++,
-                        hobby: action.hobby
-                    }
-                ]
-            };
-        case 'REMOVE_HOBBY': 
-            return {
-                ...state,
-                hobbies: state.hobbies.filter((hobby) => {
-                    return hobby.id !== action.id
-                })
-            }
-        case 'ADD_MOVIE':
-            return {
-                ...state,
-                movies:[
-                    ...state.movies,
-                    {
-                        id: nextMovieId++,
-                        title: action.title,
-                        genre: action.genre
-                    }
-                ]
-            }
-        case 'REMOVE_MOVIE':
-            return {
-                ...state,
-                movies: state.movies.filter((movie) => {
-                    return movie.id !== action.id
-                });
-            }
-        default:
-            return state; 
-    }
+// var oldReducer = (state = startingState, action) => {
+//     // state = state || {name: 'Annonymous'};
+//     switch (action.type) {
+//         case 'CHANGE_NAME':
+//             return {
+//                 ...state, 
+//                 name: action.name
+//             };
+//         case 'ADD_HOBBY':
+//             return {
+//                 ...state,
+//                 hobbies: [
+//                     ...state.hobbies,
+//                     {
+//                         id: nextHobbyId++,
+//                         hobby: action.hobby
+//                     }
+//                 ]
+//             };
+//         case 'REMOVE_HOBBY': 
+//             return {
+//                 ...state,
+//                 hobbies: state.hobbies.filter((hobby) => {
+//                     return hobby.id !== action.id
+//                 })
+//             }
+//         case 'ADD_MOVIE':
+//             return {
+//                 ...state,
+//                 movies:[
+//                     ...state.movies,
+//                     {
+//                         id: nextMovieId++,
+//                         title: action.title,
+//                         genre: action.genre
+//                     }
+//                 ]
+//             }
+//         case 'REMOVE_MOVIE':
+//             return {
+//                 ...state,
+//                 movies: state.movies.filter((movie) => {
+//                     return movie.id !== action.id
+//                 })
+//             }
+//         default:
+//             return state; 
+//     }
     
-};
+// };
 
 var nameReducer = (state = 'Annonymous', action) => {
     switch (action.type) {
@@ -89,9 +89,28 @@ var hobbiesReducer = (state = [], action) => {
     }
 }
 
+var movieReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_MOVIE':
+            return [
+                ...state,
+                    {
+                        id: nextMovieId++,
+                        title: action.title,
+                        genre: action.genre
+                    }
+            ]
+        case 'REMOVE_MOVIE':
+            return state.movies.filter((movie) =>  movie.id !== action.id);
+        default: 
+            return state; 
+    }
+}
+
 var reducer = redux.combineReducers({
     name: nameReducer,
-    hobbies: hobbiesReducer
+    hobbies: hobbiesReducer,
+    movies: moviesReducer
 })
 
 var store = redux.createStore(reducer, redux.compose(
