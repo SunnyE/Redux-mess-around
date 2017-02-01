5ceb919edc185ff37974673043f22a71418009ec
Ethan Favia <efavia215@live.com>
2017-02-01 15:26:30 -0500

    pulling in the reducers and store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -3,11 +3,14 @@ var axios = require('axios');
 
 console.log('Starting redux example');
 
-var startingState = {
-    name: "Annonymous",
-    hobbies: [],
-    movies: []
-}
+var actions = require('./actions/index');
+
+var store = require('./store/configureStore').configure();
+// var startingState = {
+//     name: "Annonymous",
+//     hobbies: [],
+//     movies: []
+// }
 
 // var oldReducer = (state = startingState, action) => {
 //     // state = state || {name: 'Annonymous'};
@@ -63,150 +66,150 @@ var startingState = {
 // name reducer and action generator. 
 
 //-------------------------------------------
-var nameReducer = (state = 'Annonymous', action) => {
-    switch (action.type) {
-        case 'CHANGE_NAME':
-            return action.name
-        default: 
-            return state; 
-    }
-};
+// var nameReducer = (state = 'Annonymous', action) => {
+//     switch (action.type) {
+//         case 'CHANGE_NAME':
+//             return action.name
+//         default: 
+//             return state; 
+//     }
+// };
 
-var changeName = (name) => {
-    return {
-        type: 'CHANGE_NAME',
-        name
-    }
+// var changeName = (name) => {
+//     return {
+//         type: 'CHANGE_NAME',
+//         name
+//     }
 
-};
+// };
 
-// hobby reducer and action generator
-//-------------------------------------------
+// // hobby reducer and action generator
+// //-------------------------------------------
 
-var nextHobbyId = 1; 
-var hobbiesReducer = (state = [], action) => {
-    switch (action.type) { 
-        case 'ADD_HOBBY':
-            return {
-                ...state,
-                hobbies: [
-                    ...state,
-                    {
-                        id: nextHobbyId++,
-                        hobby: action.hobby
-                    }
-                ]
-            };
-        case 'REMOVE_HOBBY': 
-            return state.hobbies.filter((hobby) => {hobby.id !== action.id})
-        default: 
-            return state;
-    }
-}
-var addHobby = (hobby) => {
-    return{
-        type: 'ADD_HOBBY',
-        hobby
-    };
-};
+// var nextHobbyId = 1; 
+// var hobbiesReducer = (state = [], action) => {
+//     switch (action.type) { 
+//         case 'ADD_HOBBY':
+//             return {
+//                 ...state,
+//                 hobbies: [
+//                     ...state,
+//                     {
+//                         id: nextHobbyId++,
+//                         hobby: action.hobby
+//                     }
+//                 ]
+//             };
+//         case 'REMOVE_HOBBY': 
+//             return state.hobbies.filter((hobby) => {hobby.id !== action.id})
+//         default: 
+//             return state;
+//     }
+// }
+// var addHobby = (hobby) => {
+//     return{
+//         type: 'ADD_HOBBY',
+//         hobby
+//     };
+// };
 
-var removeHobby = (id) => {
-    return {
-        type: 'REMOVE_HOBBY',
-        id
-    };
-};
+// var removeHobby = (id) => {
+//     return {
+//         type: 'REMOVE_HOBBY',
+//         id
+//     };
+// };
 
-// movie reducer and action generator
-//-------------------------------------------
-var nextMovieId = 1;
-var movieReducer = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_MOVIE':
-            return [
-                ...state,
-                    {
-                        id: nextMovieId++,
-                        title: action.title,
-                        genre: action.genre
-                    }
-            ]
-        case 'REMOVE_MOVIE':
-            return state.movies.filter((movie) =>  movie.id !== action.id);
-        default: 
-            return state; 
-    };
-};
+// // movie reducer and action generator
+// //-------------------------------------------
+// var nextMovieId = 1;
+// var movieReducer = (state = [], action) => {
+//     switch (action.type) {
+//         case 'ADD_MOVIE':
+//             return [
+//                 ...state,
+//                     {
+//                         id: nextMovieId++,
+//                         title: action.title,
+//                         genre: action.genre
+//                     }
+//             ]
+//         case 'REMOVE_MOVIE':
+//             return state.movies.filter((movie) =>  movie.id !== action.id);
+//         default: 
+//             return state; 
+//     };
+// };
 
-var addMovie = (title, genre) => {
-    return { 
-        type: 'ADD_MOVIE',
-        title,
-        genre
-    };
-};
+// var addMovie = (title, genre) => {
+//     return { 
+//         type: 'ADD_MOVIE',
+//         title,
+//         genre
+//     };
+// };
 
-var removeMovie = (id) => {
-    return {
-        type: 'REMOVE_MOVIE',
-        id
-    }
-}
+// var removeMovie = (id) => {
+//     return {
+//         type: 'REMOVE_MOVIE',
+//         id
+//     }
+// }
 
-// Map reducer and action generators 
+// // Map reducer and action generators 
 
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-    switch (action.type) {
-        case 'START_LOCATION_FETCH':
-            return {
-                isFetching:true,
-                url: undefined
-            };
-        case 'COMPLETE_LOCATION_FETCH':
-            return {
-                isFetching:false,
-                url: action.url
-            };
-        default: 
-            return state;
-    }   
+// var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+//     switch (action.type) {
+//         case 'START_LOCATION_FETCH':
+//             return {
+//                 isFetching:true,
+//                 url: undefined
+//             };
+//         case 'COMPLETE_LOCATION_FETCH':
+//             return {
+//                 isFetching:false,
+//                 url: action.url
+//             };
+//         default: 
+//             return state;
+//     }   
 
-};
+// };
 
-var startLocationFetch = () => {
-    return {
-        type: 'START_LOCATION_FETCH'
-    }
-};
+// var startLocationFetch = () => {
+//     return {
+//         type: 'START_LOCATION_FETCH'
+//     }
+// };
 
-var completeLocationFetch = (url) => {
-    return {
-        type: 'COMPLETE_LOCATION_FETCH',
-        url
-    };
-};
+// var completeLocationFetch = (url) => {
+//     return {
+//         type: 'COMPLETE_LOCATION_FETCH',
+//         url
+//     };
+// };
 
-var fetchLocation = () => {
-    store.dispatch(startLocationFetch());
+// var fetchLocation = () => {
+//     store.dispatch(startLocationFetch());
     
-    axios.get('http://ipinfo.io').then(function (res) {
-        var loc = res.data.loc;
-        var baseUrl = 'http://maps.google.com?q='
+//     axios.get('http://ipinfo.io').then(function (res) {
+//         var loc = res.data.loc;
+//         var baseUrl = 'http://maps.google.com?q='
 
-        store.dispatch(completeLocationFetch(baseUrl + loc));
-        });
-};
+//         store.dispatch(completeLocationFetch(baseUrl + loc));
+//         });
+// };
 
-var reducer = redux.combineReducers({
-    name: nameReducer,
-    hobbies: hobbiesReducer,
-    movies: moviesReducer,
-    map: mapReducer
-})
+// var reducer = redux.combineReducers({
+//     name: nameReducer,
+//     hobbies: hobbiesReducer,
+//     movies: moviesReducer,
+//     map: mapReducer
+// })
 
-var store = redux.createStore(reducer, redux.compose(
-    window.devToolsExtension ? window.devToolsExtension() : f => f
-)); 
+// var store = redux.createStore(reducer, redux.compose(
+//     window.devToolsExtension ? window.devToolsExtension() : f => f
+// )); 
 
 // subscribe to changes in the store
 var unsubscribe = store.subscribe(() => {
